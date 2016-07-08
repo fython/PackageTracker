bf94d32f3aa1fd8d1f98c6e1c8b60d2760de672f
Fung Go <fython@163.com>
2016-07-08 13:45:51 +0800

    Project: Introduce Tencent Bugly SDK
    Signed-off-by: Fung Go <fython@163.com>

---
+++ mobile/src/main/java/info/papdt/express/helper/Constants.java
@@ -0,0 +1,8 @@
+package info.papdt.express.helper;
+
+public class Constants {
+
+	public static final String BUGLY_APP_ID = "a499a51858";
+	public static final boolean BUGLY_ENABLE_DEBUG = false;
+
+}
--- mobile/src/main/java/info/papdt/express/helper/EntryActivity.java
+++ mobile/src/main/java/info/papdt/express/helper/EntryActivity.java
@@ -2,7 +2,11 @@ package info.papdt.express.helper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import com.tencent.bugly.crashreport.CrashReport;
 
 import info.papdt.express.helper.support.Settings;
 import info.papdt.express.helper.ui.MainActivity;
@@ -14,6 +18,23 @@ public class EntryActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		/** Init CrashReport */
+		CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
+		strategy.setAppPackageName(getPackageName());
+
+		String versionName = null;
+		int versionCode = 0;
+		try {
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			versionName = packageInfo.versionName;
+			versionCode = packageInfo.versionCode;
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		strategy.setAppVersion(versionName + "(" + versionCode + ")");
+		CrashReport.initCrashReport(getApplicationContext(), Constants.BUGLY_APP_ID, Constants.BUGLY_ENABLE_DEBUG, strategy);
+
+		/** Open activity */
 		Settings mSettings = Settings.getInstance(getApplicationContext());
 
 		if (mSettings.getBoolean(Settings.KEY_FIRST_RUN, true)) {
