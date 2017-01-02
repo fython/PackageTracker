c910b5ffce525704bb32cf4676f1e61abad41834
ArchieMeng <506759765@qq.com>
2017-01-02 15:43:13 +0800

    remove useless EntryActivity and use SplashActivity instead

--- mobile/src/main/java/info/papdt/express/helper/ui/SplashActivity.java
+++ mobile/src/main/java/info/papdt/express/helper/ui/SplashActivity.java
@@ -1,24 +1,37 @@
 package info.papdt.express.helper.ui;
 
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import info.papdt.express.helper.R;
 import info.papdt.express.helper.support.MaterialImageLoader;
 import info.papdt.express.helper.ui.common.AbsActivity;
 
 public class SplashActivity extends AbsActivity {
-
+	private String TAG = "express.SplashActivity";
 	private ImageView mLogoView;
 	private AppCompatTextView mTitleView;
+	/**
+	 * ATTENTION: This was auto-generated to implement the App Indexing API.
+	 * See https://g.co/AppIndexing/AndroidStudio for more information.
+	 */
+	private GoogleApiClient client;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			getWindow().getDecorView().setSystemUiVisibility(
 					Build.VERSION.SDK_INT < Build.VERSION_CODES.M
@@ -27,22 +40,23 @@ public class SplashActivity extends AbsActivity {
 			);
 			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 				getWindow().setStatusBarColor(Color.TRANSPARENT);
-			} else {
+			} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 				getWindow().setStatusBarColor(getResources().getColor(R.color.lollipop_status_bar_grey));
 			}
 			getWindow().setNavigationBarColor(getResources().getColor(R.color.lollipop_status_bar_grey));
 		}
-
-		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
 				MainActivity.launch(SplashActivity.this);
 				finish();
 			}
-		}, 3000);
+		}, 1000);
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 	}
 
 	@Override
@@ -54,4 +68,39 @@ public class SplashActivity extends AbsActivity {
 		mTitleView.animate().alpha(1f).setDuration(600).start();
 	}
 
+	/**
+	 * ATTENTION: This was auto-generated to implement the App Indexing API.
+	 * See https://g.co/AppIndexing/AndroidStudio for more information.
+	 */
+	public Action getIndexApiAction() {
+		Thing object = new Thing.Builder()
+				.setName("Splash Page") // TODO: Define a title for the content shown.
+				// TODO: Make sure this auto-generated URL is correct.
+				.setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+				.build();
+		return new Action.Builder(Action.TYPE_VIEW)
+				.setObject(object)
+				.setActionStatus(Action.STATUS_TYPE_COMPLETED)
+				.build();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client.connect();
+		AppIndex.AppIndexApi.start(client, getIndexApiAction());
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		AppIndex.AppIndexApi.end(client, getIndexApiAction());
+		client.disconnect();
+	}
 }
